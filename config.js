54ca913975336fca2b4d7d3a6bb27a811b920f1f
whitesosad <87567644+whitesosad@users.noreply.github.com>
2021-07-17 18:00:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
-const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+const CONFIG = {Xin chào,là White nàaa
+    introTitle: 'Cậu ơiiii!',
+    introDesc: `Cái web này tạo ra chỉ để nói cậu dễ thương cute cực kỳyyy`,
+    btnIntro: 'Chính là cậu á',
+    title: 'Hỏi thật nà 😙',
+    desc: 'Cậu thích tớ hôngggg ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Muốn dẫn bé đi chơiii.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //whitesosad.2005
 }
